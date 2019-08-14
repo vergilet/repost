8669b129db2ad5e23c76c20b669806dc39f499c6
Yaro <vergilet@users.noreply.github.com>
2019-08-14 10:30:28 +0300

    Authenticity Token Support (#5)
    * Merge 'form_authenticity_token' if Rails detected
    * Add 'authenticity_token' option (false - remove token from form, string - custom token )
    * Bump version to 0.2.9
    * Update bundler for CI
    * Update README file

--- lib/repost/extend_controller.rb
+++ lib/repost/extend_controller.rb
@@ -1,10 +1,20 @@
 if defined?(Rails)
   class ::ActionController::Base
 
-    def repost(*args)
-      render html: Repost::Senpai.perform(*args).html_safe
+    def repost(url, params: {}, options: {})
+      render html: Repost::Senpai.perform(
+        url,
+        params: params,
+        options: options.reverse_merge(authenticity_token: form_authenticity_token)
+      ).html_safe
     end
 
     alias :redirect_post :repost
+
   end
 end
+
+# Sinatra & Rack Protection
+# TODO
+# defined?(Sinatra::Base) && defined?(Rack::Protection::AuthenticityToken)
+# env&.fetch('rack.session', :csrf)
