b982e45e8b93d619b2dc8130e80708ada57a6a2f
vergilet <yoslavskiy@innocode.no>
2019-08-14 09:51:47 +0300

    Merge 'form_authenticity_token' if Rails detected Add 'authenticity_token' option (false - remove token from form, string - custom token ) Bump version to 0.2.9

--- lib/repost/senpai.rb
+++ lib/repost/senpai.rb
@@ -1,18 +1,21 @@
 module Repost
   class Senpai < Action
     DEFAULT_SUBMIT_BUTTON_TEXT = 'Continue'
+    DEFAULT_CHARSET = 'UTF-8'
 
     def initialize(url, params: {}, options: {})
-      @url             = url
-      @params          = params
-      @options         = options
-      @method          = options[:method] || :post
-      @form_id         = options[:form_id] || generated_form_id
-      @autosubmit      = options.fetch(:autosubmit, true)
-      @section_classes = options.dig(:decor, :section, :classes)
-      @section_html    = options.dig(:decor, :section, :html)
-      @submit_classes  = options.dig(:decor, :submit, :classes)
-      @submit_text     = options.dig(:decor, :submit, :text) || DEFAULT_SUBMIT_BUTTON_TEXT
+      @url                = url
+      @params             = params
+      @options            = options
+      @method             = options.fetch(:method, :post)
+      @authenticity_token = options.fetch(:authenticity_token, nil)
+      @charset            = options.fetch(:charset, DEFAULT_CHARSET)
+      @form_id            = options.fetch(:form_id, generated_form_id)
+      @autosubmit         = options.fetch(:autosubmit, true)
+      @section_classes    = options.dig(:decor, :section, :classes)
+      @section_html       = options.dig(:decor, :section, :html)
+      @submit_classes     = options.dig(:decor, :submit, :classes)
+      @submit_text        = options.dig(:decor, :submit, :text) || DEFAULT_SUBMIT_BUTTON_TEXT
     end
 
     def perform
@@ -27,21 +30,27 @@ module Repost
     private
 
     attr_reader :url, :params, :options, :method, :form_id, :autosubmit,
-                :section_classes, :section_html, :submit_classes, :submit_text
+                :section_classes, :section_html, :submit_classes,
+                :submit_text, :authenticity_token, :charset
+
+    def form_head
+      "<form id='#{form_id}' action='#{url}' method='#{method}' accept-charset='#{charset}'>"
+    end
 
     def form_body
       inputs = params.map do |key, value|
         "<input type='hidden' name='#{key}' value='#{value}'>"
       end
+      inputs.unshift(csrf_token) if authenticity_token
       inputs.join
-    end
-
-    def form_head
-      "<form id='#{form_id}' action='#{url}' method='#{method}'>"
     end
 
     def form_footer
       "</form>"
+    end
+
+    def csrf_token
+      "<input name='authenticity_token' value='#{authenticity_token}' type='hidden'>"
     end
 
     def no_script
