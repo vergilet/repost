e3204ac5d319f6932a18826f753b880bf9dde335
vergilet <yoslavskiy@innocode.no>
2019-10-08 02:15:21 +0300

    Improved Auth token usage for Rails apps Bump version Upd readme

--- lib/repost/extend_controller.rb
+++ lib/repost/extend_controller.rb
@@ -1,11 +1,13 @@
 if defined?(Rails)
   class ::ActionController::Base
+    AUTO_TOKEN_OPTIONS = ['auto', :auto].freeze
 
     def repost(url, params: {}, options: {})
+      authenticity_token = form_authenticity_token if AUTO_TOKEN_OPTIONS.include?(options[:authenticity_token])
       render html: Repost::Senpai.perform(
         url,
         params: params,
-        options: options.reverse_merge(authenticity_token: form_authenticity_token)
+        options: options.merge({authenticity_token: authenticity_token}.compact)
       ).html_safe
     end
 
