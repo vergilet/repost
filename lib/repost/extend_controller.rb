bc63f628627bed94ab84ac66b25736f448c21bc7
vergilet <yoslavskiy@innocode.no>
2019-10-11 23:55:11 +0300

    Lazy set up helper method

--- lib/repost/extend_controller.rb
+++ lib/repost/extend_controller.rb
@@ -1,18 +1,19 @@
-if defined?(Rails)
-  class ::ActionController::Base
-    AUTO_TOKEN_OPTIONS = ['auto', :auto].freeze
+if defined?(Rails) && defined?(ActiveSupport)
+  ActiveSupport.on_load(:action_controller) do
+    class ::ActionController::Base
 
-    def repost(url, params: {}, options: {})
-      authenticity_token = form_authenticity_token if AUTO_TOKEN_OPTIONS.include?(options[:authenticity_token])
-      render html: Repost::Senpai.perform(
-        url,
-        params: params,
-        options: options.merge({authenticity_token: authenticity_token}.compact)
-      ).html_safe
+      def repost(url, params: {}, options: {})
+        authenticity_token = form_authenticity_token if ['auto', :auto].include?(options[:authenticity_token])
+        render html: Repost::Senpai.perform(
+          url,
+          params: params,
+          options: options.merge({authenticity_token: authenticity_token}.compact)
+        ).html_safe
+      end
+
+      alias :redirect_post :repost
+
     end
-
-    alias :redirect_post :repost
-
   end
 end
 
